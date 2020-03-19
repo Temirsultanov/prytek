72bf3f3f87a6240fd6e4b44ae9329d419a3f0b85
Мухаммад <human.twm@gmail.com>
2020-03-20 00:57:37 +0300

    Адаптив 2 страниц

--- script/script.js
+++ script/script.js
@@ -1,8 +1,33 @@
 var header = document.querySelector('header');
 var onScrollHead = function () {
-    header.classList.add('black-header');
-    if (pageYOffset == 0) {
-        header.classList.remove('black-header');
-    }
+    if (!logo.classList.contains('logo-act')){
+        header.classList.add('black-header');
+        if (pageYOffset == 0) {
+            header.classList.remove('black-header');
+        };
+    };
 };
 window.addEventListener('scroll', onScrollHead);
+var burger = document.querySelector('.burger-menu');
+var burgerTop = burger.querySelector('.burger1');
+var burgerMiddle = burger.querySelector('.burger2');
+var burgerBottom = burger.querySelector('.burger3');
+var mobileMenu = document.querySelector('.menu-mobile');
+var logo = header.querySelector('.logo');
+var onBurgerClick = function () {
+    if (pageYOffset > 0) {
+        var header = document.querySelector('header');
+        header.classList.toggle('black-header');
+    };
+    burgerTop.classList.toggle('burger1-act');
+    burgerMiddle.classList.toggle('burger2-act');
+    burgerBottom.classList.toggle('burger3-act');
+    mobileMenu.classList.toggle('translate0')
+    logo.classList.toggle('logo-act');
+};
+burger.addEventListener('click', onBurgerClick);
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 767 && burgerTop.classList.contains('burger1-act')){
+       onBurgerClick(); 
+    }
+})
