a9fea9f762c329b635ff54efbe881938cf55fbdd
Мухаммад <human.twm@gmail.com>
2020-03-22 18:56:52 +0300

    Responsive

--- script/script.js
+++ script/script.js
@@ -1,13 +1,20 @@
 var header = document.querySelector('header');
 var onScrollHead = function () {
     if (!logo.classList.contains('logo-act')){
+        burgerTop.classList.remove('burger-white');
+        burgerMiddle.classList.remove('burger-white');
+        burgerBottom.classList.remove('burger-white');
         header.classList.add('black-header');
         if (pageYOffset == 0) {
             header.classList.remove('black-header');
+            burgerTop.classList.add('burger-white');
+            burgerMiddle.classList.add('burger-white');
+            burgerBottom.classList.add('burger-white');
         };
     };
 };
-window.addEventListener('scroll', onScrollHead);
+
+
 var burger = document.querySelector('.burger-menu');
 var burgerTop = burger.querySelector('.burger1');
 var burgerMiddle = burger.querySelector('.burger2');
@@ -25,6 +32,7 @@ var onBurgerClick = function () {
     mobileMenu.classList.toggle('translate0')
     logo.classList.toggle('logo-act');
 };
+window.addEventListener('scroll', onScrollHead);
 burger.addEventListener('click', onBurgerClick);
 window.addEventListener('resize', function() {
     if (window.innerWidth > 767 && burgerTop.classList.contains('burger1-act')){
